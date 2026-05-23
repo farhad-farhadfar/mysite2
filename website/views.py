9355b28a621ff318399d53058c2bef4b24c7abe4
Farhad Farhadfar <tyfarhad77@gmail.com>
2026-05-23 21:46:45 +0330

    created render html file

--- website/views.py
+++ website/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return HttpResponse('<h1>Home page</h1>')
+    return render(request,'website/index.html')
 
 def about_view(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request,'website/about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1>Contact</h1>')
+    return render(request,'website/contact.html')
 
 
